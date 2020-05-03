6ca5ee4c99c50967411d57e60adb04d6a781fb1e
Daniel Rosa <danielrosaa@gmail.com>
2020-05-03 00:47:27 -0300

    Configuring cards animation.

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,15 @@
          <searched ref="searched" />
       </header>
       <div v-show="voiceAnimationEnd" class="container">
-         <container @drop="onDrop" lock-axis="y" drop-class="drop-class">
-            <draggable v-for="invoice in invoices" :key="invoice.id">
-               <info-card
-                  :info="invoice"
-                  class="info-card"
-                  :class="invoice.active && 'active'"
-               />
-            </draggable>
-         </container>
-         <!-- <info-card class="info-card" />
-         <info-card class="info-card" />
-         <info-card class="info-card" /> -->
+         <info-card
+            :id="'infoCard' + index"
+            draggable
+            v-for="(invoice, index) in invoices"
+            :key="invoice.id"
+            :info="invoice"
+            class="info-card"
+            :class="invoice.active && 'active'"
+         />
       </div>
    </div>
 </template>
@@ -27,11 +24,11 @@ import Searched from "@/components/Searched";
 import VoiceToText from "@/components/VoiceToText";
 import InfoCard from "@/components/InfoCard";
 import { gsap } from "gsap";
+import Draggable from "gsap/Draggable";
 import { mapGetters } from "vuex";
-import { Container, Draggable } from "vue-smooth-dnd";
 export default {
    name: "App",
-   components: { Searched, VoiceToText, InfoCard, Container, Draggable },
+   components: { Searched, VoiceToText, InfoCard },
    computed: {
       ...mapGetters(["voiceAnimationEnd"]),
    },
@@ -59,26 +56,75 @@ export default {
             .from(btnImg, { opacity: 0 })
             .from(searched.$el, { opacity: 0, x: 25 }, "-=1");
       },
-      onDrop(dropResult) {
-         this.invoices = this.applyDrag(this.invoices, dropResult);
+      dragAnimation() {
+         const rearrangeInvoices = () => {
+            this.invoices.map((el, index) => {
+               if (index !== 1) {
+                  el.active = false;
+               } else {
+                  el.active = true;
+               }
+            });
+            const firstEl = this.invoices.splice(0, 1);
+            this.invoices.push(firstEl);
+            console.log(this.invoices);
+         };
+
+         gsap.registerPlugin(Draggable);
+         Draggable.create(`.info-card`, {
+            type: "y",
+            inertia: false,
+            cursor: "context-menu",
+            activeCursor: "context-menu",
+            dragResistance: 0.8,
+            zIndexBoost: false,
+            onDragEnd() {
+               if (this.y <= -20 || this.y >= 20) {
+                  gsap.to(".active", {
+                     duration: 0.5,
+                     opacity: 0.15,
+                     scale: 0.72,
+                     y: 105,
+                  });
+                  rearrangeInvoices();
+               } else {
+                  gsap.to(".active", {
+                     duration: 0.5,
+                     y: 0,
+                  });
+               }
+            },
+         });
+         // this.invoices.forEach((el, index) => {
+         //    Draggable.create(`#infoCard${index}`, {
+         //       type: "y",
+         //       inertia: false,
+         //       cursor: "context-menu",
+         //       activeCursor: "context-menu",
+         //       dragResistance: 0.8,
+         //       zIndexBoost: false,
+         //       onDragEnd() {
+         //          if (this.y <= -20 || this.y >= 20) {
+         //             gsap.to(".active", {
+         //                duration: 0.5,
+         //                opacity: 0.15,
+         //                scale: 0.72,
+         //                y: 105,
+         //             });
+         //             rearrangeInvoices();
+         //          } else {
+         //             gsap.to(".active", {
+         //                duration: 0.5,
+         //                y: 0,
+         //             });
+         //          }
+         //       },
+         //    });
+         // });
       },
-      applyDrag(arr, dragResult) {
-         const { removedIndex, addedIndex, payload } = dragResult;
-         if (removedIndex === null && addedIndex === null) return arr;
-
-         const result = [...arr];
-         let itemToAdd = payload;
-
-         if (removedIndex !== null) {
-            itemToAdd = result.splice(removedIndex, 1)[0];
-         }
-
-         if (addedIndex !== null) {
-            result.splice(addedIndex, 0, itemToAdd);
-         }
-
-         return result;
-      },
+   },
+   mounted() {
+      this.dragAnimation();
    },
    watch: {
       voiceAnimationEnd() {
@@ -113,16 +159,20 @@ header {
    display: flex;
    flex-direction: column;
    justify-content: center;
-   .info-card,
-   .smooth-dnd-draggable-wrapper {
-      // height: 100%;
+   .info-card {
+      height: 100%;
       display: flex;
       justify-self: center;
       align-self: center;
       align-items: center;
+      position: absolute;
+      &.active {
+         opacity: 1 !important;
+         transform: translateX(0);
+         scale: 1;
+      }
       @for $i from 1 through 3 {
          &:not(.active):nth-child(#{$i + 1}) {
-            position: absolute;
             z-index: -1;
             @if $i == 1 {
                opacity: 0.5;
@@ -138,15 +188,5 @@ header {
          }
       }
    }
-}
-
-// Draggable styling
-.smooth-dnd-container {
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-}
-.drop-class {
-   background: black;
 }
 </style>
